cbc398e63bc8ea5a4fffa24b4aeb7ed1d1270265
nikukulkina <nikukulkina@gmail.com>
2019-07-24 20:59:28 +0300

    Пагинация и кнопки навигации
    кнопки заработали, слайды крутятся по кругу

--- app/blocks/slider/slider.js
+++ app/blocks/slider/slider.js
@@ -1,5 +1,12 @@
 import Swiper from 'swiper';
 
 export const initialSlider = () => {
-	const slider = new Swiper('.js-slider', {slidesPerColumn: 1, slidesPerGroup: 1, slidesPerView: 1, spaceBetween: 20});
+	const slider = new Swiper('.swiper-container', {slidesPerColumn: 1, slidesPerGroup: 1, slidesPerView: 1, spaceBetween: 20, loop: true,
+		pagination: {
+			el: '.swiper-pagination',
+			type: 'bullets'},
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev'}
+	});
 };
