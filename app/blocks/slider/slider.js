134d6a2913d2d546df5dc71186d8657ec79a7aa8
nikukulkina <nikukulkina@gmail.com>
2019-07-27 14:23:58 +0300

    НОРМАЛЬНЫЕ кругляши пагинации, работают со стрелками

--- app/blocks/slider/slider.js
+++ app/blocks/slider/slider.js
@@ -2,19 +2,36 @@ import Swiper from 'swiper';
 
 export const initialSlider = () => {
 	const slider = new Swiper('.swiper-container', {slidesPerColumn: 1, slidesPerGroup: 1, slidesPerView: 1, spaceBetween: 20, loop: true,
-		pagination: {
-			el: '.swiper-pagination',
-			type: 'bullets'},
 		navigation: {
 			nextEl: '.slider__next',
 			prevEl: '.slider__prev'}
 	});
-	// const prevBtn = document.querySelector('.slider__prev');
-	// prevBtn.addEventListener('click', function () {
-	// 	slider.slidePrev();
-	// });
-	// const nextBtn = document.querySelector('.slider__next');
-	// nextBtn.addEventListener('click', function () {
-	// 	slider.slideNext();
-	// });
+
+	const dots = document.querySelectorAll('.slider__dot');
+	let i = 0;
+	const prevBtn = document.querySelector('.slider__prev');
+	const nextBtn = document.querySelector('.slider__next');
+	console.log(dots[i]);
+
+	prevBtn.addEventListener('click', function () {
+		dots[i].classList.remove('dot-active');
+		i--;
+
+		if (i < 0) {
+			i = dots.length - 1;
+		}
+		dots[i].classList.add('dot-active');
+
+	});
+
+	nextBtn.addEventListener('click', function () {
+		dots[i].classList.remove('dot-active');
+		i++;
+
+		if (i >= dots.length) {
+			i = 0;
+		}
+		dots[i].classList.add('dot-active');
+
+	});
 };
