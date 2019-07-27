ad77e839cd906b90db82f7dd11f6d30a9b3f043c
nikukulkina <nikukulkina@gmail.com>
2019-07-27 16:38:08 +0300

    Управление слайдером
    Исправлено движение точек пагинации за стрелками, также их можно тыцкать и меняются слайды

--- app/blocks/slider/slider.js
+++ app/blocks/slider/slider.js
@@ -8,30 +8,17 @@ export const initialSlider = () => {
 	});
 
 	const dots = document.querySelectorAll('.slider__dot');
-	let i = 0;
-	const prevBtn = document.querySelector('.slider__prev');
-	const nextBtn = document.querySelector('.slider__next');
-	console.log(dots[i]);
 
-	prevBtn.addEventListener('click', function () {
-		dots[i].classList.remove('dot-active');
-		i--;
-
-		if (i < 0) {
-			i = dots.length - 1;
-		}
-		dots[i].classList.add('dot-active');
-
+	slider.on('slideChange', function () {
+		console.log(slider.realIndex);
+		const d = document.querySelector('.dot-active');
+		d.classList.remove('dot-active');
+		dots[slider.realIndex].classList.add('dot-active');
 	});
 
-	nextBtn.addEventListener('click', function () {
-		dots[i].classList.remove('dot-active');
-		i++;
-
-		if (i >= dots.length) {
-			i = 0;
-		}
-		dots[i].classList.add('dot-active');
-
-	});
+	for (let i = 0; i < dots.length; i++) {
+		dots[i].addEventListener('click', function () {
+			slider.slideTo(i + 1);
+		});
+	}
 };
