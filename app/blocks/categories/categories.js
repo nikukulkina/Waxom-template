44d3fb8b593df9a01e0e434a121f863c80447278
nikukulkina <nikukulkina@gmail.com>
2019-07-31 19:49:26 +0300

    Табы
    Работают!!!

--- app/blocks/categories/categories.js
+++ app/blocks/categories/categories.js
@@ -1,11 +1,24 @@
 export const category = () => {
 	const categories = document.querySelectorAll('.categories__item');
-
 	for (let i = 0; i < categories.length; i++) {
 		categories[i].addEventListener('click', function () {
 			const activCategory = document.querySelector('.categories__item_active');
+			const id = this.getAttribute('data-category');
+			let projects = document.querySelectorAll('.project');
+			const content = document.querySelectorAll('.project[data-category = "' + id + '"]');
 			activCategory.classList.remove('categories__item_active');
 			categories[i].classList.add('categories__item_active');
+			for (let j = 0; j < projects.length; j++) {
+				projects[j].classList.add('project_hidden');
+			}
+			for (let n = 0; n < content.length; n++) {
+				content[n].classList.remove('project_hidden');
+			}
+			document.querySelector('.all').addEventListener('click', function () {
+				for (let q = 0; q < projects.length; q++) {
+					projects[q].classList.remove('project_hidden');
+				}
+			});
 		});
 	}
 	// for (const el of categories) {
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,10 +2,12 @@ import svg4everybody from 'svg4everybody';
 import $ from 'jquery';
 import '../blocks/button/button.js';
 import {initialSlider}from '../blocks/slider/slider.js';
-import {category} from '../blocks/categories/categories.js';
+import {category}from '../blocks/categories/categories.js';
+import {projects}from '../blocks/projects/projects';
 
 $(() => {
 	svg4everybody();
 	initialSlider();
+	projects();
 	category();
 });
