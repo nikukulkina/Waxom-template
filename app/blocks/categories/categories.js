89735ad46f1edded9424c594d996764f5d9c51e7
nikukulkina <nikukulkina@gmail.com>
2019-07-31 20:08:00 +0300

    Теперь нормально работают))

--- app/blocks/categories/categories.js
+++ app/blocks/categories/categories.js
@@ -1,24 +1,25 @@
 export const category = () => {
 	const categories = document.querySelectorAll('.categories__item');
 	for (let i = 0; i < categories.length; i++) {
+		document.querySelector('.all').addEventListener('click', function () {
+			let projects = document.querySelectorAll('.project');
+			for (let q = 0; q < projects.length; q++) {
+				projects[q].classList.remove('project_hidden');
+			}
+		});
 		categories[i].addEventListener('click', function () {
 			const activCategory = document.querySelector('.categories__item_active');
 			const id = this.getAttribute('data-category');
-			let projects = document.querySelectorAll('.project');
 			const content = document.querySelectorAll('.project[data-category = "' + id + '"]');
 			activCategory.classList.remove('categories__item_active');
 			categories[i].classList.add('categories__item_active');
-			for (let j = 0; j < projects.length; j++) {
-				projects[j].classList.add('project_hidden');
+			let project = document.querySelectorAll('.project');
+			for (let j = 0; j < project.length; j++) {
+				project[j].classList.add('project_hidden');
 			}
 			for (let n = 0; n < content.length; n++) {
 				content[n].classList.remove('project_hidden');
 			}
-			document.querySelector('.all').addEventListener('click', function () {
-				for (let q = 0; q < projects.length; q++) {
-					projects[q].classList.remove('project_hidden');
-				}
-			});
 		});
 	}
 	// for (const el of categories) {
