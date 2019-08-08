02dca5bddf6ae414e4488d6ef47fee75ed3fac42
nikukulkina <nikukulkina@gmail.com>
2019-08-08 16:35:55 +0300

    Блок с видео
    Все криво, но работает

--- app/blocks/video-player/video-player.js
+++ app/blocks/video-player/video-player.js
@@ -6,27 +6,56 @@ export const player = () => {
 	progressBar.value = 0;
 	progressBar.min = 0;
 	progressBar.max = video.duration;
+	console.log({video});
+	console.log(progressBar.min);
+	console.log(progressBar.max);
+	console.log(video.duration);
+	const volumeBar = document.querySelector('.video-player__scroll-bar_volume');
+	volumeBar.min = 0;
+	volumeBar.max = 100;
+	volumeBar.value = 100;
 	function initDuration() {
 		progressBar.value = video.currentTime;
 	}
 	let startDuration;
-
 	function playPauseVideo(){
 		if (video.paused) {
+			contain.classList.add('hidden');
 			video.play();
 			startDuration = setInterval(initDuration, 1000 / 66);
 		}
 		else {
 			video.pause();
-			contain.classList.toggle('hidden');
+			contain.classList.remove('hidden');
 			clearInterval(startDuration);
 		}
 	}
+	function clearDuration() {
+		clearInterval(startDuration);
+		if (video.paused) {
+			console.log(5);// линтер не пропускает пустой блок
+		}
+		else {
+			playPauseVideo();
+		}
+	}
+	function moveRange() {
+		video.currentTime = progressBar.value;
+		playPauseVideo();
+	}
+	function volumeChange() {
+		video.volume = volumeBar.value / 100;
+	}
+	const controls = document.querySelector('.video-player__controls');
 	play.addEventListener('click', function () {
-		contain.classList.add('hidden');
 		video.classList.add('video-player__window_visible');
+		controls.classList.add('video-player__controls_visible');
 		playPauseVideo();
 	});
-	// const videoVisible = document.querySelector('.video-player__window_visible');
 	video.addEventListener('click', playPauseVideo);
+	progressBar.addEventListener('mousedown', clearDuration);
+	progressBar.addEventListener('mouseup', moveRange);
+	const playBtn = document.querySelector('.video-player__play-btn');
+	playBtn.addEventListener('click', playPauseVideo);
+	volumeBar.addEventListener('mousemove', volumeChange);
 };
