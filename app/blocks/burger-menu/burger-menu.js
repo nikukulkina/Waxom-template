c973e70b679a7fe9e3774ca134b4e07bf2fa92d5
nikukulkina <nikukulkina@gmail.com>
2019-08-29 14:09:48 +0300

    Мобильное меню, его анимация

--- app/blocks/burger-menu/burger-menu.js
+++ app/blocks/burger-menu/burger-menu.js
@@ -5,5 +5,7 @@ export const burger = () => {
 		for (const item of lines) {
 			item.classList.toggle('burger-menu__line_active');
 		}
+		const menu = document.querySelector('.header__mobile-menu');
+		menu.classList.toggle('header__mobile-menu_active');
 	});
 };
