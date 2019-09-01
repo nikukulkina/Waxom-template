2f2d3af62f4c8ac3b1a0191d48586268a80d2ae2
nikukulkina <nikukulkina@gmail.com>
2019-09-02 02:58:50 +0300

    Видео работает

--- app/blocks/video-start-screen/video-start-screen.js
+++ app/blocks/video-start-screen/video-start-screen.js
@@ -2,8 +2,12 @@ export const player = () => {
 	const play = document.querySelector('.video-start-screen__play');
 	const contain = document.querySelector('.video-start-screen__contain');
 	const videoPlayer = document.querySelector('.video-youtube');
+	const videoPlayerFrame = document.querySelector('.video-youtube__frame');
+
 	play.addEventListener('click', () => {
 		contain.classList.add('video-start-screen__contain_hidden');
 		videoPlayer.classList.add('video-youtube_visible');
+		const attr = videoPlayerFrame.getAttribute('src');
+		videoPlayerFrame.setAttribute('src', attr + '&autoplay=1');
 	});
 };
